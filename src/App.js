09bead73a27649cbbba63754db2f7e6372922bbc
ariola <ariola.markou@gmail.com>
2022-09-07 12:25:02 +0300

    Fetched the data and created the question component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import Questions from "./questions";
+import data from "./data";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [question, setQuestion] = useState(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Accordion Project</h1>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">Questions and Anwers about Login</div>
+          <div className="col-9">
+            <ul>
+              {question.map((question) => (
+                <Questions key={question.id} question={question} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function questions({ question }) {
+  return <div> {question.title}</div>;
+}
+
+export default questions;
