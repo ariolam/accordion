cd6591773349da5a3956e75a9d7abb3d0e59b6f2
ariola <ariola.markou@gmail.com>
2022-09-07 12:37:13 +0300

    Fetched the questions and created the button

--- src/questions.js
+++ src/questions.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function questions({ question }) {
-  return <div> {question.title}</div>;
+  return (
+    <div className="question-container">
+      <div className="row">
+        <div className="col-9">
+          <h6> {question.title}</h6>
+        </div>
+        <div className="col-3">
+          <button> Press here </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default questions;
